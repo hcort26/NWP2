b9296e85c80ce19812a1612ddbb3e2a15ef67c7e
hcort26 <144395316+hcort26@users.noreply.github.com>
2024-04-04 18:00:05 -0400

    Update ParticipantConnection.java

--- ParticipantConnection.java
+++ ParticipantConnection.java
@@ -7,28 +7,26 @@ import java.io.IOException;
 import java.net.Socket;
 
 class ParticipantConnection implements Comparable<ParticipantConnection> {
+	
+	
+    private int points;
+    private boolean isEligibleToAnswer;
+    
     private Socket connectionSocket;
     private PrintWriter outputWriter;
     private BufferedReader inputReader;
     private String answerKey;
-    private int points;
-    private boolean isEligibleToAnswer;
 
     public ParticipantConnection(Socket conn) throws IOException {
         this.connectionSocket = conn;
+        this.isEligibleToAnswer = false;
+        
         this.outputWriter = new PrintWriter(conn.getOutputStream(), true);
         this.inputReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        
         this.points = 0;
-        this.isEligibleToAnswer = false;
+        
         broadcastMessage("points: " + points);
-    }
-
-    public void defineCorrectAnswer(String ans) {
-        this.answerKey = ans.substring(ans.length() - 1);
-    }
-
-    public void broadcastMessage(String msg) throws IOException {
-        outputWriter.println(msg);
     }
 
     public Socket getConnection() {
@@ -37,6 +35,64 @@ class ParticipantConnection implements Comparable<ParticipantConnection> {
 
     public String getParticipantID() {
         return connectionSocket.getRemoteSocketAddress().toString();
+    }
+    
+    public void defineCorrectAnswer(String ans) {
+        this.answerKey = ans.substring(ans.length() - 1);
+    }
+
+    public void broadcastMessage(String msg) throws IOException {
+        outputWriter.println(msg);
+    }
+    
+    @Override
+    public int compareTo(ParticipantConnection other) {
+        return Integer.compare(other.points, this.points);
+    }
+
+    public boolean canParticipantAnswer() {
+        return isEligibleToAnswer;
+    }
+
+    public void setEligibleToAnswer(boolean status) {
+        this.isEligibleToAnswer = status;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void disconnect() throws IOException {
+        if (inputReader != null) inputReader.close();
+        if (outputWriter != null) outputWriter.close();
+        if (connectionSocket != null) connectionSocket.close();
+    }
+    
+    private void evaluateAnswer(String participantAnswer) {
+        participantAnswer = participantAnswer.trim().substring(0, 1);
+
+        if (this.answerKey.equals(participantAnswer)) {
+            System.out.println("Participant answered correctly.");
+            points += 10;
+            try {
+                broadcastMessage("correct " + points);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.out.println("Participant answered incorrectly.");
+            points -= 10;
+            try {
+                broadcastMessage("incorrect " + points);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        try {
+            QuizServer.advanceAllToNextQuestion();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void receiveMessages() {
@@ -68,55 +124,4 @@ class ParticipantConnection implements Comparable<ParticipantConnection> {
             }
         }
     }
-
-    private void evaluateAnswer(String participantAnswer) {
-        participantAnswer = participantAnswer.trim().substring(0, 1);
-
-        if (this.answerKey.equals(participantAnswer)) {
-            System.out.println("Participant answered correctly.");
-            points += 10;
-            try {
-                broadcastMessage("correct " + points);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        } else {
-            System.out.println("Participant answered incorrectly.");
-            points -= 10;
-            try {
-                broadcastMessage("incorrect " + points);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        try {
-            QuizServer.advanceAllToNextQuestion();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Override
-    public int compareTo(ParticipantConnection other) {
-        return Integer.compare(other.points, this.points);
-    }
-
-    public boolean canParticipantAnswer() {
-        return isEligibleToAnswer;
-    }
-
-    public void setEligibleToAnswer(boolean status) {
-        this.isEligibleToAnswer = status;
-    }
-
-    public int getPoints() {
-        return points;
-    }
-
-    public void disconnect() throws IOException {
-        if (inputReader != null) inputReader.close();
-        if (outputWriter != null) outputWriter.close();
-        if (connectionSocket != null) connectionSocket.close();
-    }
 }
-
